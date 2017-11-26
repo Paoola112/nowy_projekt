5cb79ab479daa1443d2f50f34477303d79d41b4d
Paoola112 <paoola112@wp.pl>
2017-11-26 15:08:19 +0100

    czesc 1

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-imie = 'Paulina'
-print ("heloo World! zmianazmiana, {0}".format(imie))
+def wypisz_osobe (imie,nazwisko):
+    print ("{0} {1}".format (imie,nazwisko))
+
+def czy_emeryt(wiek):
+    MAX = 65
+    if wiek < MAX:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    imie = 'Paulina'
+    nazwisko = 'W'
+    wiek = 30
+    print (nazwisko)
+
+    wypisz_osobe (imie,nazwisko)
+    if czy_emeryt(wiek):
+        print('nie emeryt')
+    else:
+        print('emeryt')
